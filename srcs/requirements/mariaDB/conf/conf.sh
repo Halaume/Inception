60e0c1ce8b4dae0d24e86c672735ee224350e126
Halaume <g.hanquer@outlook.com>
2023-03-29 12:21:43 -0400

    Aled

--- srcs/requirements/mariaDB/conf/conf.sh
+++ srcs/requirements/mariaDB/conf/conf.sh
@@ -1,9 +1,12 @@
 #! /bin/bash
 
-mariadb -u root -p
+cat <<EOF >init.conf
 CREATE DATABASE wordpress
 CREATE USER 'Chef'@'localhost' IDENTIFIED BY 'Chef';
 CREATE USER 'User'@'localhost' IDENTIFIED BY 'User';
 GRANT ALL PRIVILEGES ON wordpress.* TO 'Chef'@'localhost';
 FLUSH PRIVILEGES;
 EXIT;
+EOF
+
+mysql -uroot < init.conf
